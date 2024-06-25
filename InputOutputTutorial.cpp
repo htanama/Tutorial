1bc5638106e521df07eac9e8c186cf3cf5460914
Harry <39251813+htanama@users.noreply.github.com>
2024-06-25 08:41:37 -0400

    Update InputOutputTutorial.cpp

--- InputOutputTutorial.cpp
+++ InputOutputTutorial.cpp
@@ -7,11 +7,18 @@
 int main() {
     
     std::string fileName="tempData.csv";
+    
     std::ofstream outFile(fileName);
     char delimiter = ';';
     outFile << "Apple" << delimiter << "Strawberry" << delimiter << "Banana";
+    outFile.close();
     
-    outFile.cloes();
+    std::ifstream inFile(fileName);
+    if(inFile.is_open()){
+        std::string perLine;
+        std::getline(inFile, perLine);
+    }
+    
     std::stringstream strStream;
     
     return 0;
