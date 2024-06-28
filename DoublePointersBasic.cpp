b0120c4f678cdf7f24d74eecc3498bf80cfc10d7
Harry <39251813+htanama@users.noreply.github.com>
2024-06-28 16:27:09 -0400

    Update DoublePointersBasic.cpp

--- DoublePointersBasic.cpp
+++ DoublePointersBasic.cpp
@@ -9,18 +9,23 @@ int main() {
     // double pointer variable can only point to a pointer variable
     int **dbPtrInt = &ptrInt;
     
-    cout << std::setw(25) << std::left << "address of num: " << &num << endl;
-    cout << std::setw(25) << std::left <<"address of *ptrInt: " << ptrInt << endl;
-    cout << std::setw(25) << std::left <<"address of *dbPtrInt: " << *dbPtrInt << endl << endl;
-    cout << std::setw(25) << std::left <<"address &ptrInt: "<< &ptrInt << endl;
-    cout << std::setw(25) << std::left <<"Who address is this? "<< dbPtrInt << endl << endl;
+    cout << std::setw(30) << std::left << "address of num: " << &num << endl;
+    cout << std::setw(30) << std::left <<"address of *ptrInt: " << &ptrInt << endl;
+    cout << std::setw(30) << std::left <<"address of *dbPtrInt: " << &dbPtrInt << endl << endl;
     
-    cout << std::setw(25) << std::left << "The value of num: "<< num << endl;
-    cout << std::setw(25) << std::left <<"The value of ptrInt: " << *ptrInt << endl;
-    cout << std::setw(25) << std::left << "The value of dbPtrInt: " << **dbPtrInt << endl;
+    cout << std::setw(30) << std::left << "address of num: " << &num << endl;
+    cout << std::setw(30) << std::left <<"ptrInt point to address &num "<< ptrInt << endl << endl;
     
-    ptrInt=NULL;
-    dbPtrInt=NULL; 
+    cout << std::setw(50) << std::left <<"dbPtrInt point to &ptrInt's address "<< dbPtrInt << endl;
+    
+    cout << std::setw(50) << std::left <<"dbPtrInt have access to &num's address too "<< *dbPtrInt << endl << endl;
+    
+    cout << std::setw(30) << std::left << "The value of num: "<< num << endl;
+    cout << std::setw(30) << std::left <<"The value of ptrInt: " << *ptrInt << endl;
+    cout << std::setw(30) << std::left << "The value of dbPtrInt: " << **dbPtrInt << endl;
+    
+    ptrInt=nullptr;
+    dbPtrInt=nullptr; 
     
     return 0;
 }
